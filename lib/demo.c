7f5c10ed925d088f3de4f808e7ee5eca75c1f810
Philippe <phildoreau45@gmail.com>
2020-07-05 21:44:37 +0200

    add emscripten

---
+++ lib/demo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	printf("WASM Ready - Testing with Emscripten HTML\n");
+	return 1;
+}
+
+int getNum() {
+	return 22;
+}
+
+int getDoubleNum(int n) {
+	return n * 2;
+}
+
+char * greet(char * name) {
+	char * greeting = "Hello ";
+	strcat(greeting, name);
+	return greeting;
+}
